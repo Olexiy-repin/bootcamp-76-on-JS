ac876c9c439ebb1502fe934c82ffb9be159fd8fb
Olexiy-repin <repinlyoha@gmail.com>
2025-06-12 12:58:54 +0300

    classwork

---
+++ js/01-vars.js
@@ -0,0 +1,33 @@
+/*
+ * Змінні та типи даних
+ *
+ * - Оголошення змінних з let та const
+ * - Перевизначення значення
+ * - Найменування змінних
+ * - Вибір між const і let
+ * - Виведення даних
+ * - Отримання даних
+ * - Типи даних: number, string, boolean, null, undefined, BigInt, Symbol
+ * - Оператор typeof
+ * - Суворий режим
+ */
+
+// const pricePerItem = 100;
+
+// console.log(pricePerItem);
+// alert(pricePerItem);
+
+// const userAge = prompt('Скільки вам років?');
+
+// console.log(userAge);
+
+// const username = 'Oleksii';
+
+// console.log(username);
+
+// console.log(100);
+// console.log('100');
+
+// let firstName = 'Oleksii';
+
+// console.log(firstName);
---
+++ js/03-operators.js
@@ -0,0 +1,71 @@
+/*
+ * Арифметичні операції
+ * - Оператори +, -, *, /, %, **
+ * - Комбіновані оператори
+ */
+
+// const numA = 10;
+// const numB = 4;
+
+// const result = numA ** numB;
+
+// console.log(result);
+
+/*
+10 / 6 = 6
+*/
+
+/*
+ TODO: Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+ */
+
+// const apples = 135;
+// const grapes = 47;
+
+// const total = apples + grapes;
+
+// console.log(total);
+
+// const diff = apples - grapes;
+
+// console.log(diff);
+
+/*
+ TODO: Заміни вираз перевизначення комбінованим оператором.
+ */
+//~ Без комбінованого оператора
+// let students = 100;
+
+// students = students + 50;
+
+// console.log(students);
+
+//~ З комбінованим оператором
+// let students = 100;
+
+// // students = students - 50;
+// students += 50;
+
+// console.log(students);
+
+/*
+ TODO: Розбери пріоритет операторів в інструкції привласнення значення змінної result.
+*/
+
+// const result = 108 + (223 - 2) * 5;
+
+// console.log(result);
+
+/*
+  TODO: Дано 90 хвилин, дізнайтеся скільки в цьому числі міститься годин і хвилин.
+  TODO: Результат виведіть у консоль.
+*/
+
+// const totalMinutes = 90;
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+
+// console.log(hours);
+// console.log(minutes);
+
+// console.log(hours, ':', minutes);
