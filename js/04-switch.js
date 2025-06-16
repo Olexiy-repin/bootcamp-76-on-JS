0d9544b27753a80d194b81ceb3aa54a28f0d1eac
Olexiy-repin <repinlyoha@gmail.com>
2025-06-16 09:22:52 +0300

    classwork

---
+++ js/04-switch.js
@@ -0,0 +1,40 @@
+/*
+ * Оператор switch
+ */
+
+/*
+TODO: Виконай рефакторинг коду задачі використовуючи switch.
+TODO:
+TODO: Якщо до дедлайну 0 днів - виведи рядок "Today"
+TODO: Якщо до дедлайну 1 день - виведи рядок "Tomorrow"
+TODO: Якщо до дедлайну 2 дні - виведи рядок "Overmorrow"
+TODO: Якщо до дедлайну 3+ днів - виведи рядок "Date in the future"
+ */
+
+// const daysUntilDeadline = 2;
+
+// if (daysUntilDeadline === 0) {
+//   console.log('Today');
+// } else if (daysUntilDeadline === 1) {
+//   console.log('Tomorrow');
+// } else if (daysUntilDeadline === 2) {
+//   console.log('Overmorrow');
+// } else {
+//   console.log('Date in the future');
+// }
+
+/*
+TODO: Напиши скрипт вибору опції доставки товара.
+TODO: Опція зберігається в змінній option: 1 - самовивіз, 2 - кур'єр, 3 - пошта
+TODO:
+TODO: В змінну message записати повідомлення в залежності від опції.
+TODO: - 'Ви можете забрати товар завтра з 12:00 в нашому офісі'
+TODO: - 'Кур'єр доставить замовлення завтра з 9:00 до 18:00'
+TODO: - 'Посилку буде відправлено сьогодні'
+TODO: - 'Вам передзвонить менеджер'
+ */
+
+// const option = 3;
+// let message = '';
+
+// console.log(message);
---
+++ js/05-operators.js
@@ -0,0 +1,56 @@
+/*
+ * Логічні оператори
+ *
+ * - Перетворення типів: логічне
+ * - Оператор &&
+ * - Оператор ||
+ * - Оператор !
+ */
+
+//~ 6 хибних значень: false, NaN, null, undefined, 0, ''.
+
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(0));
+// console.log(Boolean(3.14));
+// console.log(Boolean(-10));
+// console.log(Boolean(''));
+// console.log(Boolean('hello'));
+// console.log(Boolean('false'));
+
+/*
+ ~ Оператор &&
+ */
+
+/*
+ ~ Оператор ||
+ */
+
+/*
+ ~ Оператор !
+ */
+
+/*
+ TODOD: Що буде виведено до консолі?
+ */
+
+// console.log(true && 3);
+
+// console.log(false && 3);
+
+// console.log(true && 4 && 'kiwi');
+
+// console.log(true && 0 && 'kiwi');
+
+// console.log(true || 3);
+
+// console.log(true || 3 || 4);
+
+// console.log(true || false || 7);
+
+// console.log(null || 2 || undefined);
+
+// console.log((1 && null && 2) > 0);
+
+// console.log(null || (2 && 3) || 4);
