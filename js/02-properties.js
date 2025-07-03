8e0d42bfc4d5c0df933cb4972002c41dd9fed555
Olexiy-repin <repinlyoha@gmail.com>
2025-07-03 09:26:04 +0300

    preparation

---
+++ js/02-properties.js
@@ -0,0 +1,14 @@
+/*
+ * Властивості елемента (hero)
+ * - Зображення
+ * - Текст та textContent
+ * https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480
+ */
+
+/*
+ * Атрибути
+ * - get(ім'я-атрибута)
+ * - set(ім'я-атрибута, значення-атрибута)
+ * - remove(ім'я-атрибута)
+ * - has(ім'я-атрибута)
+ */
---
+++ js/06-templating.js
@@ -0,0 +1,56 @@
+const pictures = [
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/0/700/460',
+    alt: 'laptop',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/1/700/460',
+    alt: 'developer',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/10/700/460',
+    alt: 'forest',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/100/700/460',
+    alt: 'beach',
+  },
+  {
+    width: 700,
+    height: 460,
+    url: 'https://picsum.photos/id/1000/700/460',
+    alt: 'mountain',
+  },
+];
+
+// const refs = {
+//   mainTitle: document.querySelector('.js-title'),
+// };
+
+// console.log(refs);
+
+// refs.mainTitle.textContent = '<span>Main</span> gallery';
+// refs.mainTitle.innerHTML = '<span>Main</span> gallery';
+
+/*
+<li class="gallery-item">
+  <a href="#">
+    <img src="" alt="" width="" height="">
+  </a>
+</li>
+*/
+
+//~ Функція для створення карточки createGalleryCard(cardInfo)
+const createGalleryCard = pictureInfo => {};
+
+//~ Створення масиву рядків із елементами
+
+//~ Вставка елементів на сторінку
