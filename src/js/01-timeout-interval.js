26c07f963f36f2618be9583cfe7f2fc2d6d81c84
Olexiy-repin <repinlyoha@gmail.com>
2025-07-14 12:55:58 +0300

    classwork

---
+++ src/js/01-timeout-interval.js
@@ -0,0 +1,32 @@
+/*
+ * Синхронний vs Асинхронний JS
+ *
+ * Стек виклику функцій (http://latentflip.com/loupe/)
+ * window.setTimeout(callback, delay);
+ * window.setInterval(callback, delay);
+ * Очищення інтервалів та таймутів clearInterval(intervalID), clearTimeout(timeoutID)
+ */
+
+// console.log('Start');
+
+// const timeoutId = setTimeout(function () {
+//   console.log('Hello from setTimeout!');
+// }, 5000);
+
+// document.addEventListener('click', event => {
+//   clearTimeout(timeoutId);
+// });
+
+// console.log('End');
+
+// console.log('Start');
+
+// const intervalId = setInterval(() => {
+//   console.log('Hello from setInterval!');
+// }, 1000);
+
+// document.addEventListener('click', event => {
+//   clearInterval(intervalId);
+// });
+
+// console.log('End');
